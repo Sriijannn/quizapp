3f1d3adf3d62cd75ec1c157aad0867e5a6ada762
Srijan Tripathi <developer.srijan@gmail.com>
2024-11-01 17:34:38 +0530

    Added few more routes

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Question = require("../models/questions");
 const router = express.Router();
 
+// Route to inject questions to the database.
 router.post("/feedques", async (req, res) => {
   const questions = req.body.questions;
 
@@ -14,4 +15,23 @@ router.post("/feedques", async (req, res) => {
   }
 });
 
+// Route to send the questions, not yet secure.
+router.post("/getquestions", async (req, res) => {
+  const set = req.body.set;
+  try {
+    quesArray = await questions.findOne(set);
+    res.status(201).json(quesArray);
+  } catch (error) {
+    console.error("Error finding the set", error);
+    res.status(500).json({ error: "Error finding the set" });
+  }
+});
+
+// Route for the basic info of the user. has to be secure? jwt verification?
+router.get("/getinfo");
+
 module.exports = router;
+
+// route for the timer? nope
+
+// Post request for submission of answers,
